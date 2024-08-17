fedd9edd50dff01241b1b3ff32b43abab5e7e258
parvin <parvinnoori0@gmail.com>
2024-08-17 15:04:44 +0330

    overlay

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 export default function Overlay() {
   return (
-    <div className="h-full w-full fixed bg-black opacity-80 top-0 right-0 z-40 duration-700 transition-all">
-      Overlay
-    </div>
+    <div className="h-full w-full fixed bg-black opacity-80 top-0 right-0 z-40 duration-700 transition-all"></div>
   );
 }
