21b06b23d752ace35bab14f5e18c8f14292d3a79
parvin <parvinnoori0@gmail.com>
2024-09-17 07:54:12 +0330

    insert oulet

--- src/layouts/identityLayout/IdentityLayout.jsx
+++ src/layouts/identityLayout/IdentityLayout.jsx
@@ -1,12 +1,10 @@
-import { FormCol } from "@/components/FormCol";
-import { ImgCol } from "@/components/imgCol";
+import { Outlet } from "react-router-dom";
 
 export default function IdentityLayout() {
   return (
     <div className="flex h-100svh w-100svw items-center justify-center bg-gray-400">
       <div className="grid lg:grid-cols-2 xl:w-3/5  sm:w-4/5 w-full h-full bg-white">
-        <FormCol />
-        <ImgCol />
+        <Outlet />
       </div>
     </div>
   );
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,11 @@
-
+import { FormCol } from "@/components/FormCol";
+import { ImgCol } from "@/components/imgCol";
 
 export default function SignUP() {
   return (
-    <div>SignUP</div>
-  )
+    <>
+      <FormCol />
+      <ImgCol />
+    </>
+  );
 }
