cc1a7b2322ab51c41ebb60af8b510f38cc2b5ccb
parvin <parvinnoori0@gmail.com>
2024-10-05 21:07:57 +0330

    improve UI:img col component

--- src/components/imgCol/ImgCol.jsx
+++ src/components/imgCol/ImgCol.jsx
@@ -2,8 +2,8 @@ import bannerImg from "../../assets/imgs/signUp-img.png";
 
 export default function ImgCol() {
   return (
-    <div className="xl:col-span-6 col-span-7 hidden lg:flex h-full justify-end overflow-hidden  items-center relative before:content[''] before:bg-primaryOrange before:size-[47rem] before:-z-0 before:block before:absolute before:aspect-square before:-right-48 before:-top-14 before:rounded-full">
-      <img src={bannerImg} alt="banner" className="h-[50rem] z-10 mt-10"/>
+    <div className="xl:col-span-6 h-full  col-span-7 hidden lg:flex  justify-end overflow-hidden  items-center relative before:content[''] before:bg-primaryOrange before:h-full before:-z-0 before:block before:absolute before:aspect-square before:-right-48 before:-top-14 before:rounded-full">
+      <img src={bannerImg} alt="banner" className="h-full  z-10 mt-10"/>
     </div>
   );
 }
